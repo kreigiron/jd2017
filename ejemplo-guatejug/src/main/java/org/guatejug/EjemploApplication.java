5d3733f8b23f1dea5e3a585aa355b33ec24347c0
Erik Giron <erik.giron@xoom.com>
2017-06-22 13:42:04 -0700

    Initial commit

---
+++ ejemplo-guatejug/src/main/java/org/guatejug/EjemploApplication.java
@@ -0,0 +1,12 @@
+package org.guatejug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApplication.class, args);
+	}
+}
